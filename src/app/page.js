c6a82490b11cf1b35be4498d1b8038bb2dc82040
Kanish Kumar <kanishkumar11@outlook.in>
2024-11-07 22:34:08 +0530

    completed

--- src/app/page.js
+++ src/app/page.js
@@ -17,7 +17,7 @@ export default function page() {
           alt=""
           width={500}
           height={500}
-          className="absolute left-0 top-[10%] w-[250px] lg:block hidden"
+          className="absolute left-0 top-[10%] w-[220px] lg:block hidden"
           initial={{ opacity: 0, x: 0, y: 0 }}
           animate={{ opacity: 1, x: 0, y: "10%" }}
           transition={{ ease: "easeInOut", type: "spring" }}
@@ -27,7 +27,7 @@ export default function page() {
           alt=""
           width={500}
           height={500}
-          className="absolute left-[8%] top-[23%] w-[250px] lg:block hidden"
+          className="absolute left-[8%] top-[23%] w-[220px] lg:block hidden"
           initial={{ opacity: 0, x: 0, y: 0 }}
           animate={{ opacity: 1, x: "8%", y: "0%" }}
           transition={{ ease: "easeInOut", type: "spring" }}
@@ -37,7 +37,7 @@ export default function page() {
           alt=""
           width={500}
           height={500}
-          className="absolute left-[18%] top-[35%] w-[250px] lg:block hidden"
+          className="absolute left-[18%] top-[35%] w-[220px] lg:block hidden"
           initial={{ opacity: 0, x: 0, y: 0 }}
           animate={{ opacity: 1, x: "0%", y: "10%" }}
           transition={{ ease: "easeInOut", type: "spring" }}
@@ -52,26 +52,22 @@ export default function page() {
           animate={{ opacity: 1, x: "0%", y: "10%" }}
           transition={{ ease: "easeInOut", type: "spring" }}
         />
-        <MotionImg
-          src="/5.svg"
-          alt=""
-          width={500}
-          height={500}
-          className="absolute right-[8%] top-[23%] w-[250px] lg:block hidden"
-          initial={{ opacity: 0, x: 0, y: 0 }}
+        <motion.div
+          className="bg-white rounded-2xl py-14 px-8  flex-col gap-4 absolute right-[3%] top-[23%] w-[320px] rotate-[-20deg] lg:flex hidden"
+          initial={{ opacity: 0, x: 0, y: 0, rotate: "12deg" }}
           animate={{ opacity: 1, x: "0%", y: "10%" }}
           transition={{ ease: "easeInOut", type: "spring" }}
-        />
-        <MotionImg
-          src="/6.svg"
-          alt=""
-          width={500}
-          height={500}
-          className="absolute right-[18%] top-[35%] w-[250px] lg:block hidden"
-          initial={{ opacity: 0, x: 0, y: 0 }}
-          animate={{ opacity: 1, x: "0%", y: "10%" }}
-          transition={{ ease: "easeInOut", type: "spring" }}
-        />
+        >
+          <h3 className="text-2xl">Yes you in line</h3>
+          <p className="text-pred">you have waitlisted successfully!</p>
+          <Image
+            src="/gif.webp"
+            width={500}
+            height={500}
+            className="bg-cover"
+            alt=""
+          />
+        </motion.div>
       </div>
       <div className="bg-pred mt-20 py-40 text-7xl text-center text-white">
         Contact the CEO
